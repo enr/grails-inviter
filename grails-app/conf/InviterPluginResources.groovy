f0cdba62d7fe44f2182260b705400df70044c26d
enr <enrico.comiti@gmail.com>
2012-10-26 12:17:42 +0200

    move providers images to plugin resources

--- grails-app/conf/InviterPluginResources.groovy
+++ grails-app/conf/InviterPluginResources.groovy
@@ -2,5 +2,14 @@ modules = {
     'inviter' {
         resource url:  [dir: 'js', file: 'inviter.js', plugin: 'inviter']
         resource url:  [dir: 'css', file: 'inviter.css', plugin: 'inviter']
+        // images for default providers
+        resource url:  [dir: 'images', file: 'facebook.png', plugin: 'inviter']
+        resource url:  [dir: 'images', file: 'google.png', plugin: 'inviter']
+        resource url:  [dir: 'images', file: 'linkedin.png', plugin: 'inviter']
+        resource url:  [dir: 'images', file: 'twitter.png', plugin: 'inviter']
+        resource url:  [dir: 'images', file: 'windowslive.png', plugin: 'inviter']
+        resource url:  [dir: 'images', file: 'yahoo.png', plugin: 'inviter']
+        // image for contacts without their own pic
+        resource url:  [dir: 'images', file: 'empty.png', plugin: 'inviter']
     }
 }
